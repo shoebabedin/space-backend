99f2ce079c24d35e21bc3d588e22dd267c39e960
shoebabedin <shoeb.abedin@intelli.global>
2023-07-04 19:10:36 +0600

    update

--- test_insapce.sql
+++ test_insapce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 03, 2023 at 03:13 PM
+-- Generation Time: Jul 04, 2023 at 03:09 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -24,6 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `blogs`
+--
+
+CREATE TABLE `blogs` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `blog_Img` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `blogs`
+--
+
+INSERT INTO `blogs` (`id`, `title`, `content`, `blog_Img`) VALUES
+(12, 'Architecture 01', 'Duis non erat elementum, finibus lectus bibendum, tristique nunc. Fusce et nunc ac ex scelerisque ultrices. Vestibulum sed velit eu mauris tempus sodales. Cras eu mi nec magna mattis dapibus eget ac erat. Vestibulum finibus, turpis ac suscipit feugiat, quam sapien elementum risus, ac rhoncus sem augue id dui. Aliquam maximus laoreet sollicitudin. In commodo tellus tellus, at iaculis lectus varius at. Fusce lacinia ante et nisi pretium pellentesque. Donec accumsan at lectus sit amet blandit. Cras vitae congue dui, id efficitur sem. Integer gravida, massa id varius ornare, velit sapien hendrerit felis, vitae sollicitudin tellus tellus id odio. Sed vel nibh laoreet, viverra ipsum a, pretium nibh. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec erat metus. Vivamus in nisl tempor, blandit justo in, accumsan enim.', '[\"files-1688474084047.jpg\",\"files-1688474084049.jpg\"]');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `home`
+--
+
+CREATE TABLE `home` (
+  `id` int(11) NOT NULL,
+  `company_name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `google_map` varchar(255) NOT NULL,
+  `image` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -31,20 +67,33 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `u_name` varchar(255) NOT NULL
+  `u_name` varchar(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `u_name`) VALUES
-(27, 'd1@gmail.com', '$2b$10$yz1OH1EIzpSs8HE3VOLmfezKjLn46Finiub8RiSwAo9uLwbqF0R2y', 'd1'),
-(28, 'shoeb@gmail.com', '$2b$10$t.IFtHd/Zwbc1XcF6hMkCes41VM8JaG.x5FlldbA4AeX0eHeEI0Qa', 'shoeb');
+INSERT INTO `users` (`id`, `email`, `password`, `u_name`, `image`) VALUES
+(27, 'd1@gmail.com', '$2b$10$yz1OH1EIzpSs8HE3VOLmfezKjLn46Finiub8RiSwAo9uLwbqF0R2y', 'd1', 'file-1688466836157.jpg'),
+(28, 'shoeb@gmail.com', '$2b$10$t.IFtHd/Zwbc1XcF6hMkCes41VM8JaG.x5FlldbA4AeX0eHeEI0Qa', 'shoeb', 'file-1688465760031.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `blogs`
+--
+ALTER TABLE `blogs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `home`
+--
+ALTER TABLE `home`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -55,6 +104,18 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `blogs`
+--
+ALTER TABLE `blogs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `home`
+--
+ALTER TABLE `home`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
